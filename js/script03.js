aead29a1aa5e7d971f0a481793f21c6f9ca03386
Gabriel <gabriel.mgran20@hotmail.com>
2023-06-06 16:11:11 -0300

    update
    Colocando textos e imagens na pagina.

--- js/script03.js
+++ js/script03.js
@@ -7,17 +7,17 @@ function pesquisar () {
     img.setAttribute('id', 'foto')
     
     switch(recordes) {
-        case '' :
-          res.innerHTML = "<h1> </h1>  "
-          img.setAttribute('src', '')
+        case 'Guerra' :
+          res.innerHTML = "<h1>Parou a Guerra</h1> <br> <p> No dia 4 de fevereiro de 1969, uma terça-feira na cidade de Benin, que se encontrava em meio a uma Guerra Civil, ambas as partes concordaram em fazer uma pausa no conflito para assistir ao jogo do Santos. Esse dia ficou conhecido como 'O Dia em que o Santos parou a guerra'.<p> "
+          img.setAttribute('src', 'imagens/recordes/guerra.jpg')
           break;
-        case '' :
-            res.innerHTML = "<h1></h1>  " 
-            img.setAttribute('src', '')
+        case 'Eleito pela Fifa' :
+            res.innerHTML = "<h1> Quinto Maior Clube Século XX </h1> <br> <p> O Santos foi eleito pela FIFA, em 2000, como o quinto maior clube de futebol do século XX, sendo considerado o melhor clube das Américas nessa lista. Além disso, no ano do seu centenário, a FIFA concedeu ao Santos o título de 'maior clube sul-americano do século XX' em uma cerimônia realizada na Câmara dos Deputados em Brasília. <p>  " 
+            img.setAttribute('src', 'imagens/recordes/quinto.jpg')
             break;
-        case '' :
-            res.innerHTML = "<h1></h1> " 
-            img.setAttribute('src', '')
+        case 'Pentacampeão' :
+            res.innerHTML = "<h1>Pentacampeão Brasileiro</h1> <br> <p> No dia 8 de dezembro de 1965, uma quarta-feira, o Santos venceu o Vasco no Maracanã e conquistou o Campeonato Brasileiro. Essa vitória marcou a quinta conquista consecutiva do clube (1961, 1962, 1963, 1964 e 1965), tornando-se a primeira e única vez na história em que um clube brasileiro alcançou tal feito. <p> " 
+            img.setAttribute('src', 'imagens/recordes/penta.jpg')
             break;
             
     
@@ -38,4 +38,4 @@ function pesquisar () {
     
     }
 
-    res.style.lineHeight = `30px`
+    
